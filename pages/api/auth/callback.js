abc60eaeba2f2f5f4a680c35ef99989310847163
Iberê <ibere82@gmail.com>
2021-07-20 18:44:57 -0300

    mais ajustes no callback

--- pages/api/auth/callback.js
+++ pages/api/auth/callback.js
@@ -3,14 +3,33 @@ import cookie from 'cookie'
 export default async function callback(req, res) {
   const { code } = req.query
 
-  const fetchToken = await fetch(`https://github.com/login/oauth/access_token?client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}&code=${code}`, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json'
-    }
-  })
-  const bodyFetchToken = await fetchToken.json()
-  const { access_token } = bodyFetchToken
+  try {
+    const fetchToken = await fetch(`https://github.com/login/oauth/access_token?client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}&code=${code}`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    const bodyFetchToken = await fetchToken.json()
+    const { access_token } = bodyFetchToken
+
+    res.setHeader("Set-Cookie", cookie.serialize("token", access_token, {
+      httpOnly: true,
+      secure: true,//process.env.NODE_ENV !== "development",
+      maxAge: 60 * 60,
+      sameSite: "strict",
+      path: "/",
+    }))
+    res.redirect('/')
+
+
+
+
+  } catch (error) {
+    const message = `Deu erro na função callback: ${error.message} /// fim da mensagem de erro.
+    Tentativa de fetch com ID ${process.env.GITHUB_CLIENT_ID}  e code ${code}`
+    res.send(message)
+  }
 
   // const fetchUser = await fetch('https://api.github.com/user', {
   //   headers: {
@@ -21,13 +40,6 @@ export default async function callback(req, res) {
   // const user = await fetchUser.json()
   // console.log(JSON.stringify(user))
 
-  res.setHeader("Set-Cookie", cookie.serialize("token", access_token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    maxAge: 60 * 60,
-    sameSite: "strict",
-    path: "/",
-  }))
-  res.redirect('/')
+  return
 }
 
